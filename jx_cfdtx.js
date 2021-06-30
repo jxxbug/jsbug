f6d5851753095311bf823fed68847c2932806b76
galabug <153454219@qq.com>
2021-06-30 15:13:10 +0800

    jx_cfdtx

--- jx_cfdtx.js
+++ jx_cfdtx.js
@@ -29,8 +29,8 @@ $.userName = '';
   if (!getCookies()) return;
   if (!getTokens()) return;
   for (let i = 0; i < $.cookieArr.length; i++) {
-    $.currentCookie = $.cookieArr[i];
-    $.currentToken = $.tokenArr[i];
+    $.currentCookie = $.cookieArr[i]||'';
+    $.currentToken = $.tokenArr[i]||'';
     if ($.currentCookie) {
       $.userName =  decodeURIComponent($.currentCookie.match(/pt_pin=(.+?);/) && $.currentCookie.match(/pt_pin=(.+?);/)[1]);
       $.log(`\n开始【京东账号${i + 1}】${$.userName}`);
